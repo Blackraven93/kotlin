11a105198e79bcc0729b7b06cac52477b0fc67e2
Blackraven93 <reblackraven@gmail.com>
2022-12-22 23:35:03 +0900

    feat(function): change argument order using parameter type

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,11 +8,23 @@ fun readInt(): Int {
     return readLine()!!.toInt()
 }
 
+fun swap(s: String, from: Int, to:Int): String {
+    val chars = s.toCharArray()
+    val tmp = chars[from]
+    chars[from] = chars[to]
+    chars[to] = tmp
+    return chars.concatToString()
+}
+
 fun main() {
-    print("Enter radius: ")
-    val radius = readLine()!!.toDouble()
-    println("Circle area: ${circleArea(radius)}")
-    println(readInt())
+    println(swap("Hello", 1, 2))
+    println(swap("Hello", from = 1, to = 2))
+    println(swap("Hello", to = 3, from = 0))
+    println(swap("Hello", 1, to = 3))
+    println(swap(s = "Hello", 1, to = 3))
+    // 인자 순서를 변경할 시 입력하려면 모두 입력하던가
+    // 아니면 뒤의 타입을 생략해도 괜찮다.
+//    println(swap(s = "Hello", 1, from = 3))
 }
 
 
