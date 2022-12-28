e51c46f33bc1ec06559c44abd8ab3150bc615808
Blackraven93 <reblackraven@gmail.com>
2022-12-28 23:52:51 +0900

    feat(recursive): add binary classification feature using recursive

---
+++ src/main/kotlin/iterable.kt
@@ -0,0 +1,12 @@
+fun parseIntNumber(s: String, fallback: Int = -1): Int {
+    var num = 0
+
+    if (s.length !in 1..31) return fallback
+
+    for (c in s) {
+        if (c !in '0'..'1' ) return fallback
+        num = num*2 + (c - '0')
+    }
+
+    return num
+}
