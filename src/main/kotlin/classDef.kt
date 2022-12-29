06cd300ca6e633f14437daf4d93c95f82fc71fe0
Blackraven93 <reblackraven@gmail.com>
2022-12-30 00:22:30 +0900

    feat(constructor): add support constructor

--- src/main/kotlin/classDef.kt
+++ src/main/kotlin/classDef.kt
@@ -1,25 +1,16 @@
 import java.lang.IllegalArgumentException
 import java.util.IllegalFormatCodePointException
 
-class Person(var firstName: String, fullName: String) {
-    var familyName: String
-
-
-    fun fullName() = "$firstName $familyName"
-
-
-
-    init {
-        val names = fullName.split(" ")
-        if (names.size !=2 ) {
-            throw IllegalArgumentException("Invalid name: ${fullname()}")
-        }
-        firstName = names[0]
-        familyName = names[1]
+class Person {
+    val fullName: String
+    constructor(firstName: String, familyName: String):
+            this("$firstName $familyName")
+    constructor(fullName: String) {
+        this.fullName = fullName
     }
 }
 
 fun main() {
     val person = Person("Black Raven")
-    println(person.firstName)
+
 }
