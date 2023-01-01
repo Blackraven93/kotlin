3988811088f948672fd4891e6c3901bbebfa66ac
Blackraven93 <reblackraven@gmail.com>
2023-01-01 23:38:13 +0900

    feat(this): add inner class

--- src/main/kotlin/classDef.kt
+++ src/main/kotlin/classDef.kt
@@ -5,6 +5,12 @@ class Person(
     private val firstName: String,
     private val familyName: String
 ) {
+    inner class Possession(val description: String) {
+        fun showOwner() = println(fullName())
+
+        // 외부 클래스 인스턴스
+        fun getOwner() = this@Person
+    }
     fun fullName() = "$firstName $familyName"
 }
 
